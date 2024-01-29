5d8edfecd7cb567542cb74ec3dae3fc716706450
Cristian <cristianjcastano@gmail.com>
2024-01-29 15:35:51 -0500

    first commit

---
+++ src/routes/song.router.js
@@ -0,0 +1,15 @@
+const { getAll, create, getOne, destroy, update } = require('../controllers/song.controller');
+const express = require('express');
+
+const songRouter = express.Router();
+
+songRouter.route("/")
+		.get(getAll)
+        .post(create)
+
+songRouter.route('/:id')
+    .get(getOne)
+    .delete(destroy)
+    .put(update)
+
+module.exports = songRouter;
